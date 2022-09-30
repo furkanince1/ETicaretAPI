14ac60996a70df9da4d4439317aef16a820e01b9
lenovo <iincefurkann@gmail.com>
2022-10-01 02:42:11 +0300

    Proje dosyası ekle.

---
+++ Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _productService.GetProducts();
+            return Ok(products);
+        }
+
+
+    }
+}
